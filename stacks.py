797a4107bf8a1d22b7b47f546f47da939c4e5021
Mary-Njiru <marynwanjiruu@gmail.com>
2024-03-31 13:16:15 +0300

    add stack

---
+++ stacks.py
@@ -0,0 +1,11 @@
+#Creating a Stack
+stack1 = []
+stack1.append('a')
+stack1.append('b')
+stack1.append('c')
+stack1.append('d')
+stack1.append('e')
+print(stack1)
+
+#Accessing an element in a stack
+print(stack1[3])
